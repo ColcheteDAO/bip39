46ddb13f5f3cfb1c403c25c228f4b92e50e7ecc4
juancolchete <juanudkp@gmail.com>
2026-01-08 15:15:53 -0300

    feat: add sha.js hash

--- src/js/bip85-js/index.cjs
+++ src/js/bip85-js/index.cjs
@@ -7,7 +7,85 @@ const secp256k1 = new EC('secp256k1')
 
 
 var inherits = require('inherits');
-var shaHash = require('./node_modules/sha.js/hash');
+var toBuffer = require('to-buffer');
+
+// prototype class for hash functions
+function shaHashHash(blockSize, finalSize) {
+	this._block = Buffer.alloc(blockSize);
+	this._finalSize = finalSize;
+	this._blockSize = blockSize;
+	this._len = 0;
+}
+
+shaHashHash.prototype.update = function (data, enc) {
+	/* eslint no-param-reassign: 0 */
+	data = toBuffer(data, enc || 'utf8');
+
+	var block = this._block;
+	var blockSize = this._blockSize;
+	var length = data.length;
+	var accum = this._len;
+
+	for (var offset = 0; offset < length;) {
+		var assigned = accum % blockSize;
+		var remainder = Math.min(length - offset, blockSize - assigned);
+
+		for (var i = 0; i < remainder; i++) {
+			block[assigned + i] = data[offset + i];
+		}
+
+		accum += remainder;
+		offset += remainder;
+
+		if ((accum % blockSize) === 0) {
+			this._update(block);
+		}
+	}
+
+	this._len += length;
+	return this;
+};
+
+shaHashHash.prototype.digest = function (enc) {
+	var rem = this._len % this._blockSize;
+
+	this._block[rem] = 0x80;
+
+	/*
+	 * zero (rem + 1) trailing bits, where (rem + 1) is the smallest
+	 * non-negative solution to the equation (length + 1 + (rem + 1)) === finalSize mod blockSize
+	 */
+	this._block.fill(0, rem + 1);
+
+	if (rem >= this._finalSize) {
+		this._update(this._block);
+		this._block.fill(0);
+	}
+
+	var bits = this._len * 8;
+
+	// uint32
+	if (bits <= 0xffffffff) {
+		this._block.writeUInt32BE(bits, this._blockSize - 4);
+
+		// uint64
+	} else {
+		var lowBits = (bits & 0xffffffff) >>> 0;
+		var highBits = (bits - lowBits) / 0x100000000;
+
+		this._block.writeUInt32BE(highBits, this._blockSize - 8);
+		this._block.writeUInt32BE(lowBits, this._blockSize - 4);
+	}
+
+	this._update(this._block);
+	var hash = this._hash();
+
+	return enc ? hash.toString(enc) : hash;
+};
+
+shaHashHash.prototype._update = function () {
+	throw new Error('_update must be implemented by subclass');
+};
 
 var K = [
 	0x5a827999, 0x6ed9eba1, 0x8f1bbcdc | 0, 0xca62c1d6 | 0
@@ -30,10 +108,10 @@ function Sha() {
 	this.init();
 	this._w = W;
 
-	shaHash.call(this, 64, 56);
+	shaHashHash.call(this, 64, 56);
 }
 
-inherits(Sha, shaHash);
+inherits(Sha, shaHashHash);
 
 Sha.prototype.init = function () {
 	this._a = 0x67452301;
