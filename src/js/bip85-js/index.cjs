1f3073c14e3ac75528ba1f3bb1b9ff49ad175878
juancolchete <juanudkp@gmail.com>
2026-01-12 15:48:54 -0300

    feat: add hash.js utils

--- src/js/bip85-js/index.cjs
+++ src/js/bip85-js/index.cjs
@@ -9,8 +9,35 @@ let BN = require('bn.js')
 var HmacDRBG = require('hmac-drbg');
 var utils = require('./node_modules/elliptic/lib/elliptic/utils');
 
-let utilsHash = require('./node_modules/hash.js/lib/hash/utils');
+function utilsHashToHex32(msg, endian) {
+  var res = '';
+  for (var i = 0; i < msg.length; i++) {
+    var w = msg[i];
+    if (endian === 'little')
+      w = htonl(w);
+    res += zero8(w.toString(16));
+  }
+  return res;
+}
 
+function utilsHashSplit32(msg, endian) {
+  var res = new Array(msg.length * 4);
+  for (var i = 0, k = 0; i < msg.length; i++, k += 4) {
+    var m = msg[i];
+    if (endian === 'big') {
+      res[k] = m >>> 24;
+      res[k + 1] = (m >>> 16) & 0xff;
+      res[k + 2] = (m >>> 8) & 0xff;
+      res[k + 3] = m & 0xff;
+    } else {
+      res[k + 3] = m >>> 24;
+      res[k + 2] = (m >>> 16) & 0xff;
+      res[k + 1] = (m >>> 8) & 0xff;
+      res[k] = m & 0xff;
+    }
+  }
+  return res;
+}
 
 function SHA384() {
   if (!(this instanceof SHA384))
@@ -37,9 +64,9 @@ SHA384.padLength = 128;
 
 SHA384.prototype._digest = function digest(enc) {
   if (enc === 'hex')
-    return utilsHash.toHex32(this.h.slice(0, 12), 'big');
+    return utilsHashToHex32(this.h.slice(0, 12), 'big');
   else
-    return utilsHash.split32(this.h.slice(0, 12), 'big');
+    return utilsHashSplit32(this.h.slice(0, 12), 'big');
 };
 
 var curve = exports;
